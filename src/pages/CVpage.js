7263deab0dc53c1143b6f05ede64ccedd4175cb7
Zeroexe <exe_6084@hotmail.com>
2020-06-20 02:09:19 -0400

    removing seo component from others page

--- src/pages/CVpage.js
+++ src/pages/CVpage.js
@@ -1,11 +1,9 @@
 import React from "react"
 import Layout from "../components/layout/layout"
-import SEO from "../components/seo"
 import CVPresentation from "../components/curriculum/CVPresentation"
 
 const CVpage = () => (
   <Layout>
-    <SEO title="Curriculum Vitae"/>
     <h1>Curriculum Vitae</h1>
     <div>
       <CVPresentation></CVPresentation>
--- src/pages/index-2.js
+++ src/pages/index-2.js
@@ -2,11 +2,9 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout/layout"
-import SEO from "../components/seo"
 
 const IndexPage2 = () => (
   <Layout>
-    <SEO title="Home" />
     <h1>Zeroexe's blog</h1>
     <p>En este blog se espera plasmar distintos fases del conocimiento adquirido a lo largo de mi camino en la programación.</p>
     <p>Aunque mi pasion se encuentra en los videojuegos actualmente soy desarrollador web en una empresa, tomo esto como un paso para mucho mas adelante poder tener una empresa sustentable de videojuegos</p>
