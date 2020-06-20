f87a780f7dd5e95db4cfbfbb5ba57eaae6a51f69
Zeroexe <exe_6084@hotmail.com>
2020-06-20 02:06:04 -0400

    other fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,28 @@
 import React from "react"
+import { useStaticQuery, graphql } from 'gatsby'
 
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
+const NotFoundPage = () => {
+  const data = useStaticQuery(graphql`
+  {
+    site {
+        siteMetadata {
+          title
+          description
+          author
+        }
+    }
+  }
+`)
+
+return (
   <Layout>
-    <SEO title="404: Not found" />
+    <SEO title="404: Not found" site={data.site} />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
-)
+)}
 
 export default NotFoundPage
